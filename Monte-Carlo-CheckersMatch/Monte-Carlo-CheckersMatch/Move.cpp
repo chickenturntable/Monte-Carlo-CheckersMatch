d418cb84adb0688466dfcc89236434df328b4792
Samuel A. Cobb <samuelcobbac@gmail.com>
2018-04-13 10:56:26 -0500

    Configuration

---
+++ Monte-Carlo-CheckersMatch/Monte-Carlo-CheckersMatch/Move.cpp
@@ -0,0 +1,20 @@
+/********************************************************************************************************
+Author: Samuel A. Cobb
+Description:
+*********************************************************************************************************/
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <bitset>
+#include <algorithm>
+#include <map>
+#include <stack>
+#include <array>
+#include <list>
+#include <math.h>
+#include <ctime>
+#include <random>
+#include <ctime>
+
+using namespace std;
---
+++ Monte-Carlo-CheckersMatch/Monte-Carlo-CheckersMatch/Move.h
@@ -0,0 +1,25 @@
+#pragma once
+/********************************************************************************************************
+Author: Samuel A. Cobb
+Description:
+*********************************************************************************************************/
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <bitset>
+#include <algorithm>
+#include <map>
+#include <stack>
+#include <array>
+#include <list>
+#include <math.h>
+#include <ctime>
+#include <random>
+#include <ctime>
+
+using namespace std;
+
+class Move {
+
+};
